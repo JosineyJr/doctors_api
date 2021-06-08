a556758a940002dd99e24f2d329e81ae566be268
Josiney Junior <josineyjunior14@gmail.com>
2021-06-08 19:59:50 -0300

    refactor: 💡 add method
    add method findBySpecialty to interface

--- src/modules/doctors/repositories/IDoctorsRepository.ts
+++ src/modules/doctors/repositories/IDoctorsRepository.ts
@@ -10,4 +10,6 @@ export default interface IDoctorsRepository {
   findByCellPhone(cellPhone: string): Promise<Doctor>;
 
   findByLandline(landline: string): Promise<Doctor[]>;
+
+  findBySpecialties(specialty: string): Promise<Doctor[]>;
 }
