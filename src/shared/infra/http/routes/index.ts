d9e4d025ed56d21f2bedf581e932c537e93346d1
Josiney Junior <josineyjunior14@gmail.com>
2021-06-08 20:26:33 -0300

    refactor: 💡 specialties routes
    add specialtiesRoutes to routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import doctorsRoutes from '@modules/doctors/infra/http/routes/doctorsRoutes';
 import cepsRoutes from '@modules/CEP/infra/http/routes/cepsRoutes';
+import specialtiesRoutes from '@modules/specialties/infra/http/routes/specialtiesRoutes';
 
 const routes = Router();
 
 routes.use('/doctor', doctorsRoutes);
 routes.use('/cep', cepsRoutes);
+routes.use('/specialty', specialtiesRoutes);
 
 export default routes;
