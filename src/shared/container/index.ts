4a8ecb3ec86447565ffb4f4afea6e0b43765d491
Josiney Junior <josineyjunior14@gmail.com>
2021-06-09 17:38:36 -0300

    feat: 🎸 inverting dependency
    inverting dependency of specialties repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,11 +2,18 @@ import CepsRepository from '@modules/CEP/infra/typeorm/repositories/CepsReposito
 import CepProvider from '@modules/CEP/providers/CepProvider/implementations/CepProvider';
 import ICepProvider from '@modules/CEP/providers/CepProvider/models/ICepProvider';
 import ICepsRepository from '@modules/CEP/repositories/ICepsRepository';
+import SpecialtiesRepository from '@modules/specialties/infra/typeorm/repositories/SpecialtiesRepository';
+import ISpecialtiesRepository from '@modules/specialties/repositories/ISpecialtiesRepository';
 import DoctorsRepository from '@modules/doctors/infra/typeorm/repositories/DoctorsRepository';
 import IDoctorsRepository from '@modules/doctors/repositories/IDoctorsRepository';
 import { container } from 'tsyringe';
 
 container.registerSingleton<ICepsRepository>('CepsRepository', CepsRepository);
+
+container.registerSingleton<ISpecialtiesRepository>(
+  'SpecialtiesRepository',
+  SpecialtiesRepository,
+);
 
 container.registerSingleton<IDoctorsRepository>(
   'DoctorsRepository',
