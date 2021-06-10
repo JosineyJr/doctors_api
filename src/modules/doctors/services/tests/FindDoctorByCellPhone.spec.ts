6c83961df93437d74622de77b50171e7f794a1d2
Josiney Junior <josineyjunior14@gmail.com>
2021-06-10 19:00:34 -0300

    test: 💍 unit test to doctor service
    add unit test to find doctor by cell phone service

---
+++ src/modules/doctors/services/tests/FindDoctorByCellPhone.spec.ts
@@ -0,0 +1,65 @@
+import faker from 'faker';
+import FakeCepProvider from '@modules/cep/providers/CepProvider/Fakes/FakeCepProvider';
+import FakeCepsRepository from '@modules/cep/repositories/Fakes/FakeCepsRepository';
+import RegisterCepService from '@modules/cep/services/RegisterCepService';
+import FakeDoctorsRepostiory from '@modules/doctors/repositories/fakes/FakeDoctorsRepository';
+import AppError from '@shared/errors/AppError';
+import Doctor from '@modules/doctors/infra/typeorm/entities/Doctor';
+import FakeSpecialtiesRepository from '@modules/specialties/repositories/Fakes/FakeSpecialtiesRepository';
+import CreateDoctorService from '../CreateDoctorService';
+import FindDoctorByCellPhoneService from '../FindDoctorByCellPhone';
+
+let fakeDoctorsRepository: FakeDoctorsRepostiory;
+let fakeSpecialtiesRepository: FakeSpecialtiesRepository;
+let fakeCepsRepository: FakeCepsRepository;
+
+let fakeCepProvider: FakeCepProvider;
+
+let createDoctorService: CreateDoctorService;
+let registerCepService: RegisterCepService;
+let findByCellPhoneService: FindDoctorByCellPhoneService;
+
+let createdDoctor: Doctor;
+
+describe('FindByCellPhone', () => {
+  beforeEach(async () => {
+    fakeDoctorsRepository = new FakeDoctorsRepostiory();
+    fakeSpecialtiesRepository = new FakeSpecialtiesRepository();
+    fakeCepsRepository = new FakeCepsRepository();
+
+    fakeCepProvider = new FakeCepProvider();
+
+    registerCepService = new RegisterCepService(
+      fakeCepsRepository,
+      fakeCepProvider,
+    );
+    createDoctorService = new CreateDoctorService(
+      fakeDoctorsRepository,
+      fakeSpecialtiesRepository,
+      registerCepService,
+    );
+    findByCellPhoneService = new FindDoctorByCellPhoneService(
+      fakeDoctorsRepository,
+    );
+    createdDoctor = await createDoctorService.execute({
+      cellPhone: '31999999999',
+      cep: '35163143',
+      crm: '1234567',
+      name: faker.name.firstName(),
+      specialties: ['Alergologia', 'Angiologia'],
+      landline: faker.phone.phoneNumber(),
+    });
+  });
+  it('should not be able to find the doctor by cell phone', async () => {
+    await expect(findByCellPhoneService.execute('crm')).rejects.toBeInstanceOf(
+      AppError,
+    );
+  });
+  it('should be able to find the doctor by id', async () => {
+    const findById = await findByCellPhoneService.execute(
+      createdDoctor.cellPhone,
+    );
+    expect(findById.cep.cep).toEqual('35163143');
+    expect(findById.cellPhone).toEqual('31999999999');
+  });
+});
