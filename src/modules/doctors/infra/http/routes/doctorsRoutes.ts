24006f064cd365916c4ec0e02c5a34cd9a63616f
Josiney Junior <josineyjunior14@gmail.com>
2021-06-10 23:04:22 -0300

    refactor: 💡 set required
    set field as required

--- src/modules/doctors/infra/http/routes/doctorsRoutes.ts
+++ src/modules/doctors/infra/http/routes/doctorsRoutes.ts
@@ -12,11 +12,11 @@ doctorsRoutes.post(
   celebrate({
     [Segments.BODY]: {
       name: Joi.string().required(),
-      crm: Joi.string().regex(validations.crm),
-      cellPhone: Joi.string().regex(validations.cellPhone),
+      crm: Joi.string().regex(validations.crm).required(),
+      cellPhone: Joi.string().regex(validations.cellPhone).required(),
       landline: Joi.string().regex(validations.landline),
-      cep: Joi.string().regex(validations.cep),
-      specialties: Joi.array().items(Joi.string()).min(2),
+      cep: Joi.string().regex(validations.cep).required(),
+      specialties: Joi.array().items(Joi.string()).min(2).required(),
     },
   }),
   doctorsController.create,
@@ -32,6 +32,24 @@ doctorsRoutes.get(
     },
   }),
   doctorsController.index,
+);
+
+doctorsRoutes.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string(),
+      crm: Joi.string().regex(validations.crm),
+      cellPhone: Joi.string().regex(validations.cellPhone),
+      landline: Joi.string().regex(validations.landline),
+      cep: Joi.string().regex(validations.cep),
+      specialties: Joi.array().items(Joi.string()).min(2),
+    },
+  }),
+  doctorsController.update,
 );
 
 export default doctorsRoutes;
