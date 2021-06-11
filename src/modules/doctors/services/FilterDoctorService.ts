c7ab6b372e300c82259de57cf4cabd0297f6b444
Josiney Junior <josineyjunior14@gmail.com>
2021-06-10 21:02:24 -0300

    feat: 🎸 cell phone and landline filter
    add cell phone and landliner filters to filter doctor service

--- src/modules/doctors/services/FilterDoctorService.ts
+++ src/modules/doctors/services/FilterDoctorService.ts
@@ -1,8 +1,11 @@
 import { inject, injectable } from 'tsyringe';
 import { validate } from 'uuid';
+import validations from '@config/valitadions';
 import Doctor from '../infra/typeorm/entities/Doctor';
 import FindDoctorByCrmService from './FindDoctorByCrmService';
 import FindByDoctorIdService from './FindDoctorByIdService';
+import FindDoctorByCellPhoneService from './FindDoctorByCellPhoneService';
+import FindDoctorByLandLineService from './FindDoctorByLandlineService';
 
 @injectable()
 class FilterDoctorService {
@@ -11,12 +14,22 @@ class FilterDoctorService {
     private findDoctorByIdService: FindByDoctorIdService,
     @inject('FindDoctorByCrmService')
     private findDoctorByCrmService: FindDoctorByCrmService,
+    @inject('FindDoctorByCellPhoneService')
+    private findDoctorByCellPhoneService: FindDoctorByCellPhoneService,
+    @inject('FindDoctorByLandLineService')
+    private findDoctorByLandLineService: FindDoctorByLandLineService,
   ) {}
 
-  public async execute(param: string): Promise<Doctor> {
+  public async execute(param: string): Promise<Doctor | Doctor[]> {
     if (validate(param)) return this.findDoctorByIdService.execute(param);
 
-    return this.findDoctorByCrmService.execute(param);
+    if (validations.crm.test(param))
+      return this.findDoctorByCrmService.execute(param);
+
+    if (Number(param.charAt(2)) >= 6)
+      return this.findDoctorByCellPhoneService.execute(param);
+
+    return this.findDoctorByLandLineService.execute(param);
   }
 }
 
