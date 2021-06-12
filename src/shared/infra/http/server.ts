8bea9099ce87c670920913ae3995768d77b14784
Josiney Junior <josineyjunior14@gmail.com>
2021-06-11 23:39:41 -0300

    refactor: 💡 add link to docs
    add link to open api docs

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,5 +4,5 @@ const port = process.env.SERVER_PORT || 3031;
 
 app.listen(port, () => {
   console.log(`🔥️ Server started on port ${port}`);
-  console.log(`🔥️ Open documentation on http://localhost:${port}/docs-api`);
+  console.log(`🔥️ Open documentation on http://localhost:${port}/api-docs`);
 });
